dec0f1c01c6b2eaf9bae15b6b03468879d47202f
irenewn <irenewemonan@gmail.com>
2019-11-21 19:40:55 +0700

    nambah gui

--- demo.py
+++ demo.py
@@ -13,15 +13,20 @@ root.title("Logo Recognition")
 #         content = file.read()
 #         print(content)
 
+
 def open():
     global my_image
     root.filename = filedialog.askopenfilename(title="Select a file",filetypes=(("jpg files","*.jpg"),("all types","*.*")))
-    my_image = ImageTk.PhotoImage(Image.open(root.filename))
+    my_image = ImageTk.PhotoImage(Image.open(root.filename).resize((200,200), Image.ANTIALIAS))
     my_image_label = Label(root, image=my_image)
     my_image_label.place(x=80, y=200)
 
 label_0 = Label(root, text="Logo Recognition",width=20,font=("bold",20))
 label_0.place(x=90, y=53)
+
+label_1 = Label(root, text="Logo Recognition",width=20,font=("bold",20))
+label_1.place(x=90, y=450)
+
 
 b1 = Button(root, text="Select Picture",width=12,command=open)
 b1.place(x=80, y=130)
